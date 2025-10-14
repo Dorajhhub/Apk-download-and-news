d45da53a9661e1cbc6b8ac2bf544bac86d024e59
Dorajhhub <148616506+Dorajhhub@users.noreply.github.com>
2025-10-14 11:14:43 +0000

    test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <main className="p-8 text-center">
+            <h1>🚀 Jihucompany 공식 사이트</h1>
+            <p>React + Vite 기반 리뉴얼 중입니다.</p>
+        </main>
+    )
+}
+
+export default App;
