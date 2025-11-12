a2e72bd7bea59322d59e481fc73deeca060b0891
Dorajhhub <148616506+Dorajhhub@users.noreply.github.com>
2025-11-12 09:43:37 +0000

    test

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    // ⚠️ v4에서는 이 별도 패키지를 사용해야 합니다.
-    tailwindcss: {}, 
-    
-    // Autoprefixer는 PostCSS에서 제거해도 되지만, 일단 두셔도 무방합니다.
-    autoprefixer: {},
-  },
-} 
