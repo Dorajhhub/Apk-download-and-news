03fa7645748c179db86f957c31c6138119a4294a
Dorajhhub <148616506+Dorajhhub@users.noreply.github.com>
2025-11-12 17:09:09 +0900

    config.js 업데이트

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const servermaintenance = false;
-export const maintenanceEndTime = "2025-09-28T18:00:00"; // YYYY-MM-DDTHH:MM:SS format (year-month-day hour:minute:second)
+export const servermaintenance = true;
+export const maintenanceEndTime = "2025-11-12T19:00:00"; // YYYY-MM-DDTHH:MM:SS format (year-month-day hour:minute:second)
